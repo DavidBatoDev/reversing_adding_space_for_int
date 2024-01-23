21f8bf4eee3dceca6d532c0571d0befc0f75f8ee
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-23 16:10:11 +0800

    Finished Code

--- exercise11.py
+++ exercise11.py
@@ -1,4 +1,10 @@
 # create a function, that take integer as a parameter
-# convert the given parameter to a string
-# use string indexing technique to reverse the given sting
-# add spaces to each item
+def space_reverse_int(num):
+    # convert the given parameter to a string
+    # use string indexing technique to reverse the given sting
+    reverse_str = str(num)[::-1]
+    # add spaces to each item
+    return ' '.join(reverse_str)
+
+
+print(space_reverse_int(7536))
