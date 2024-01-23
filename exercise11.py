a7b5dec362d678ffa372aedfa6a14d61da8426ec
David E. Bato bato <149306718+DavidBatoDev@users.noreply.github.com>
2024-01-23 16:06:16 +0800

    Done with pseudocode

---
+++ exercise11.py
@@ -0,0 +1,4 @@
+# create a function, that take integer as a parameter
+# convert the given parameter to a string
+# use string indexing technique to reverse the given sting
+# add spaces to each item
